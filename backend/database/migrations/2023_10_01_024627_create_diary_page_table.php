cd7c96b3fe66d2ba9340c25a92d73eb8f4373712
Gener Echeverría <56045434+GenerEcheverria@users.noreply.github.com>
2023-09-30 20:49:15 -0600

    Entidad diary page

---
+++ backend/database/migrations/2023_10_01_024627_create_diary_page_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('diary_page', function (Blueprint $table) {
+            $table->bigIncrements('idPage');
+            $table->integer('idDiary');
+            $table->date('date');
+            $table->text('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('diary_page');
+    }
+};
