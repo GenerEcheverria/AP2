b67c34f838e49d9d55f6c3902c612949c62fd96f
Gener Echeverría <56045434+GenerEcheverria@users.noreply.github.com>
2023-10-21 14:30:56 -0600

    show current month

--- frontend/src/app/pages/diario/diario.component.ts
+++ frontend/src/app/pages/diario/diario.component.ts
@@ -1,42 +1,57 @@
 import { Component } from '@angular/core';
 import { DiarioEmbarazada } from 'src/app/interfaces/diario-embarazada';
 import { DiarioEmbarazadaService } from 'src/app/services/diario-embarazada.service';
+import { DatePipe } from '@angular/common'; // Importa DatePipe
 
 @Component({
   selector: 'app-diario',
   templateUrl: './diario.component.html',
-  styleUrls: ['./diario.component.css']
+  styleUrls: ['./diario.component.css'],
+  providers: [DatePipe], // Agrega DatePipe como un proveedor
 })
 export class DiarioComponent {
   protected verForm: boolean;
   protected pages: DiarioEmbarazada[] = [];
-  protected currentPage:  DiarioEmbarazada | undefined;
+  protected currentPage: DiarioEmbarazada | undefined;
+  protected currentDate: string; 
 
-  constructor(private diarioEmbarazadaService: DiarioEmbarazadaService) {
+  constructor(
+    private diarioEmbarazadaService: DiarioEmbarazadaService,
+    private datePipe: DatePipe // Inyecta DatePipe
+  ) {
     this.verForm = false;
-    this.listPages()
+    this.currentDate = this.formatDate(new Date()); 
+    this.listPages();
   }
 
   protected listPages() {
-    this.diarioEmbarazadaService.getHojasDiarioById(localStorage.getItem('idPatient') || "").subscribe(
-      pages => {
-        this.pages = pages as DiarioEmbarazada[]
-      }
-    )
+    this.diarioEmbarazadaService
+      .getHojasDiarioById(localStorage.getItem('idPatient') || '')
+      .subscribe((pages) => {
+        this.pages = pages as DiarioEmbarazada[];
+      });
   }
 
-  protected showDetails(idPage:number){
-    this.currentPage = this.pages.find(page => page.idPage === idPage);
+  protected siguienteMes() {
+    console.log(1);
+  }
+
+  protected showDetails(idPage: number) {
+    this.currentPage = this.pages.find((page) => page.idPage === idPage);
     this.verForm = false;
   }
-  
+
   protected deletePage(idPage: number) {
     this.diarioEmbarazadaService.deletePage(idPage).subscribe(() => {
       this.listPages();
     });
   }
-  
+
   protected visualizarForm() {
     this.verForm = true;
+  }
+
+  private formatDate(date: Date): string {
+    return this.datePipe.transform(date, 'MMMM y') || "";
   }
 }
