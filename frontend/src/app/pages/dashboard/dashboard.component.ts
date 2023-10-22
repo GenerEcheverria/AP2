a065f049566e2577bb5f3317b2eafbfedca5568b
Gener Echeverría <56045434+GenerEcheverria@users.noreply.github.com>
2023-10-22 13:35:14 -0600

    options

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { CitaService } from '../../services/cita.service';
 import { UpcomingDate } from '../../interfaces/upcoming-date';
+import { DatepickerOptions } from 'ng2-datepicker';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,36 +12,52 @@ import { UpcomingDate } from '../../interfaces/upcoming-date';
 export class DashboardComponent {
   protected name!: string;
   protected date!: string;
-  protected idUser!: string|null;
-  protected userRole!: string|null;
+  protected idUser!: string | null;
+  protected userRole!: string | null;
+  //Patient
   protected upcomingDate!: UpcomingDate;
-
+  //Doctor
   protected calendarDate!: Date;
+  protected options!: DatepickerOptions;
 
-  constructor(private authService: AuthService, private citaService: CitaService){}
+  constructor(private authService: AuthService, private citaService: CitaService) { }
 
   ngOnInit(): void {
     this.idUser = localStorage.getItem('idUser')
     this.getUserInfo()
-    this.upcomingAppointment();
     this.userRole = localStorage.getItem('role');
+    if (this.userRole == "Doctor") {
+      this.calendarDate = new Date()
+      this.options = {
+        placeholder: 'Selecciona una fecha', 
+        format: 'LLLL do yyyy', 
+        formatTitle: 'LLLL yyyy',
+        formatDays: 'EEEEE',
+        firstCalendarDay: 1, 
+        position: 'bottom',
+        calendarClass: 'datepicker-default', 
+        scrollBarColor: '#dfe3e9',
+      }
+    } else {
+      this.upcomingAppointment();
+    }
   }
 
-  private getUserInfo(){
+  private getUserInfo() {
     this.authService.me().subscribe(data => {
       this.name = data.name;
     })
   }
 
-  private upcomingAppointment(){
-    if(this.idUser != null){
+  private upcomingAppointment() {
+    if (this.idUser != null) {
       this.citaService.getUpcomingAppointment(this.idUser).subscribe(data => {
-        
+
         this.upcomingDate = data as UpcomingDate;
         this.upcomingDate.time = this.upcomingDate.time.split(':').slice(0, 2).join(':');
 
       });
     }
-    
+
   }
 }
