a1107505f98b4d60d3ef5666f010b0ef767af870
Gener Echeverría <56045434+GenerEcheverria@users.noreply.github.com>
2023-11-17 14:52:13 -0600

    update

--- backend/database/seeders/PatientSeeder.php
+++ backend/database/seeders/PatientSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Appoinment;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Patient;
@@ -16,7 +17,6 @@ class PatientSeeder extends Seeder
      */
     public function run()
     {
-        // Crear un paciente de ejemplo
         User::create([
             'id' => 3,
             'name' => 'María Fernanda García Pérez',
@@ -44,7 +44,6 @@ class PatientSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now(),
         ]);
-
         MedicalRecord::create([
             'idMedRec' => 1,
             'idPatient' => 1,
@@ -52,5 +51,62 @@ class PatientSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
+
+        #Ejemplo 2 solo para llenar datos
+        User::create([
+            'id' => 4,
+            'name' => 'Robin Paola Chi Suaréz',
+            "sex" => "Mujer",
+            "phone" => "9981792122",
+            "email"=>"robin@gmail.com",
+            // Miembarazo1234
+            'password' => '$2y$10$iWAjMLJgMgSARb/ffQOy8.8OU8Qzda5TjqIhLz5PzEFSQsEalT6Vu',
+            "role"=>"Patient",
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        $patient = Patient::create([
+            'idPatient' => 2,
+            'idUser' => 4,
+            'age' => 30,
+            'curp' => 'GAPM950101MDFRNR00',
+            'maritalStatus' => 'Soltera', 
+            'occupation' => 'Historiadora',
+            'state' => 'Yucatán',
+            'municipality' => 'Mérida',
+            'locality' => 'Mérida',
+            'address' => 'Calle 90 por 17 #221 Américas',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        MedicalRecord::create([
+            'idMedRec' => 2,
+            'idPatient' => 2,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Appoinment::create([
+            'idAppointment' => 1,
+            'idPatient' => 2,
+            'idDoctor' => 2,
+            'date' => '2023-11-17',
+            'time' => '19:00:00',
+            'summary' => '',
+            'prescription' => '',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Appoinment::create([
+            'idAppointment' => 2,
+            'idPatient' => 2,
+            'idDoctor' => 2,
+            'date' => '2023-11-18',
+            'time' => '19:30:00',
+            'summary' => '',
+            'prescription' => '',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
